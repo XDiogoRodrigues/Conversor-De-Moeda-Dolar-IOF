8fcb8e4769f08f36a729bb073ecf68bccad44bd1
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-01-15 22:44:48 -0300

    Declarado a classe ConversorDolar e seus atributos e seu método construtor

---
+++ ConversorMoedaDolar/ConversorDolar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorMoedaDolar
+{
+    class ConversorDolar
+    {
+        public double Value { get; set; }
+        public double QuoteValue { get; set; }
+
+        public ConversorDolar(double valueDolar, double quoteValue)
+        {
+            Value = valueDolar;
+            QuoteValue = quoteValue;
+        }
+    }
+}
